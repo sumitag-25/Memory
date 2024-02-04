766d15f4be2cda8a9f3d7c8a8f009943ca9c8d43
sumitag-25 <156592907+sumitag-25@users.noreply.github.com>
2024-02-04 08:20:40 +0000

    memory-spaces

---
+++ memories/new.js
@@ -0,0 +1,24 @@
+// Memories
+
+// Stack(Premetive) and Heap(Non-premetive) memory
+
+// let myName = "sumitverma"
+// let myNewName = myName
+// myNewName = "sumitkumar"
+
+// console.log(myName)
+// console.log(myNewName)
+
+let user = {
+    email: "sumitverma@google.com",
+    upi: "sumit@ybl"
+}
+
+let newUser = user
+newUser.email = "sumitverma@gmail.com"
+
+console.log(user.email)
+console.log(newUser.email)
+
+
+
